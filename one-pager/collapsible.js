3e1ad0cfd1444ca02aab738e578c9b75adc6eb61
Tobias Zelders <tobias.zelders@epitech.eu>
2026-04-01 17:05:12 +0200

    feature: fixed styling

--- one-pager/collapsible.js
+++ one-pager/collapsible.js
@@ -7,10 +7,8 @@ for (i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-      content.style.padding = 0;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      content.style.padding = 30 + "px";
     }
   });
 }
